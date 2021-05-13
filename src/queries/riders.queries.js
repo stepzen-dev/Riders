21b1d214fa500a51255d2bc3cff86538c3ac31af
samuelghill <samuelgarretthill@gmail.com>
2021-05-13 07:23:38 -0500

    Get Riders naming convention

--- src/queries/riders.queries.js
+++ src/queries/riders.queries.js
@@ -1,49 +1,49 @@
 import { gql } from "@apollo/client";
 
 export const GET_RIDER = gql`
-  query GetRidersById($id: ID!) {
-    rider(id: $id) {
-      first_name
-      id
-      last_name
-      rideSlugs {
-        slug
-        myRides {
-          continent
-          countries
-          description
-          height
-          id
-          image
-          slug
-          title
-          updated_at
-        }
-      }
-    }
-  }
+	query GetRidersById($id: ID!) {
+		rider(id: $id) {
+			first_name
+			id
+			last_name
+			rideSlugs {
+				slug
+				myRides {
+					continent
+					countries
+					description
+					height
+					id
+					image
+					slug
+					title
+					updated_at
+				}
+			}
+		}
+	}
 `;
 
 export const GET_RIDERS = gql`
-  query GetRidersById {
-    riders {
-      first_name
-      id
-      last_name
-      rideSlugs {
-        slug
-        myRides {
-          continent
-          countries
-          description
-          height
-          id
-          image
-          slug
-          title
-          updated_at
-        }
-      }
-    }
-  }
+	query GetRiders {
+		riders {
+			first_name
+			id
+			last_name
+			rideSlugs {
+				slug
+				myRides {
+					continent
+					countries
+					description
+					height
+					id
+					image
+					slug
+					title
+					updated_at
+				}
+			}
+		}
+	}
 `;
